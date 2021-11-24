acb41e0accda54b52704b90e44f29c23a78e0cb2
frontMAX <maxbjornkga@gmail.com>
2021-11-24 13:06:38 +0100

    firtst page, hover words. comments js and html

--- script.js
+++ script.js
@@ -1,20 +1,67 @@
 window.addEventListener('load', main);
-//  /** @type {HTMLElement} */
+/** @type {HTMLElement} */
 let Hello; 
 /** @type {HTMLElement} */
-let about
+let about;
+/** @type {HTMLElement} */
+let thisIs;
+/** @type {HTMLElement} */
+let work;
+/** @type {HTMLElement} */
+let myName;
+/** @type {HTMLElement} */
+let  contact;
+/** @type {HTMLElement} */
+let frontEnd;
+/** @type {HTMLElement} */
+let portfolio;
 /** start of the program */
 function main(){
  Hello = document.getElementById('Hello');
- about = document.getElementById('aboutMe')
+ about = document.getElementById('aboutMe');
+ thisIs = document.getElementById('thisIs');
+ work = document.getElementById('workHistori');
+ myName = document.getElementById('myName');
+ contact = document.getElementById('myContact');
+ frontEnd = document.getElementById('frontEnd');
+ portfolio = document.getElementById('portfolio');
 }
-/** Hover over Hello text and change that to About me */
-function infoAboutMe () {
+/** hide Hello And show About me */
+function hideHello () {
         Hello.style.display = 'none'
         aboutMe.style.display = 'block'
    }
-/** hover out and the the text Hello shows again */
-function showeAgain () {
+/** show Hello again and hide About me */
+function showeHello () {
     Hello.style.display = 'block'
     aboutMe.style.display = 'none'
+}
+/** hide This is and show Experience  */
+function hideThisIs(){
+    thisIs.style.display = 'none'
+    work.style.display = 'block'
+}
+/** show This is again and hide Experience */
+function showThisIs(){
+thisIs.style.display = 'block'
+work.style.display = 'none'
+}
+/** Hide name and show Contact */
+function hideName(){
+    myName.style.display = 'none'
+    contact.style.display = 'block'
+}
+/** show name again and hide Experience  */
+function showName(){
+    myName.style.display = 'block'
+    contact.style.display = 'none'
+}
+/** hide frontEnd and show portfolio */
+    function FontEnd (){
+    frontEnd.style.display = 'none'
+    portfolio.style.display = 'block'
+}
+function devoloper (){
+    frontEnd.style.display = 'block'
+    portfolio.style.display = 'none'
 }
