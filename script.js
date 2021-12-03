3e03903a16aab5e36360a9a3875d23a1c9a15dad
frontMAX <maxbjornkga@gmail.com>
2021-12-03 12:21:21 +0100

    new dropmenu and last changes

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 window.addEventListener('load', main);
-/** @type {HTMLElement} */
+/** @type {HTMLElement} hover and change the word to about me */
 let Hello; 
-/** @type {HTMLElement} */
+/** @type {HTMLElement} frome Hello and click go to about me page */
 let about;
-/** @type {HTMLElement} */
+/** @type {HTMLElement} hover and change to experience  */
 let thisIs;
-/** @type {HTMLElement} */
+/** @type {HTMLElement}  from this is to experience and click go to experience page*/
 let work;
-/** @type {HTMLElement} */
+/** @type {HTMLElement} hover Max and change to contact */
 let myName;
-/** @type {HTMLElement} */
+/** @type {HTMLElement} contact and click go to contact page */
 let  contact;
-/** @type {HTMLElement}  */
+/** @type {HTMLElement}  hover front end to portfolio */
 let frontEnd;
-/** @type {HTMLElement} portfolio page*/
+/** @type {HTMLElement} click and go to portfolio page*/
 let portfolio;
-/** burgerMenu @type {HTMLElement} */
-let navMenu;
-/** @type {HTMLElement} */
-let myLinks;
-/** start of the program */
+
+/** start of the program and get all the id */
 function main(){
     
  Hello = document.getElementById('Hello');
@@ -35,54 +32,62 @@ function main(){
 }
 /** hide Hello And show About me */
 function hideHello () {
-        Hello.style.display = 'none'
-        aboutMe.style.display = 'block'
-   }
+    Hello.style.display = 'none'
+    aboutMe.style.display = 'block'
+}
 /** show Hello again and hide About me */
 function showeHello () {
     Hello.style.display = 'block'
     aboutMe.style.display = 'none'
 }
 /** hide This is and show Experience  */
-function hideThisIs(){
+function hideThisIs() {
     thisIs.style.display = 'none'
     work.style.display = 'block'
 }
 /** show This is again and hide Experience */
-function showThisIs(){
+function showThisIs() {
 thisIs.style.display = 'block'
 work.style.display = 'none'
 }
 /** Hide name and show Contact */
-function hideName(){
+function hideName() {
     myName.style.display = 'none'
     contact.style.display = 'block'
 }
 /** show name again and hide Experience  */
-function showName(){
+function showName() {
     myName.style.display = 'block'
     contact.style.display = 'none'
 }
 /** hide frontEnd and show portfolio */
-    function FontEnd (){
+    function FontEnd () {
     frontEnd.style.display = 'none'
     portfolio.style.display = 'block'
 }
 /** show frontEnd again */
-function devoloper (){
+function devoloper () {
     frontEnd.style.display = 'block'
     portfolio.style.display = 'none'
 }
-/** dropMenu */
-function menuDown (){
-   navMenu.style.display = 'block'
-    navMenu.style.display = 'none'
-    myLinks.style.display = 'block'
-}
 
-function menuUpp (){
-    myLinks.style.display = 'block'
-    myLinks.style.display = 'none'
-    navMenu.style.display = 'block'
-}
+
+/** drop menu  */
+const menuBtn = document.getElementById('menu-btn')
+let menuOpen = false;
+menuBtn.addEventListener ('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        document.getElementById('navbar').classList.add('show')
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        document.getElementById('navbar').classList.remove('show')
+        menuOpen = false;
+    }
+});
+
+
+
+
 
